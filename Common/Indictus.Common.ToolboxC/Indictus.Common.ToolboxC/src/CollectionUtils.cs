cd202c326983f0648fad1d8b38c37076216d384d
Osterberg <magnus.osterberg@capgemini.com>
2017-11-20 14:50:59 +0100

    first commit

---
+++ Common/Indictus.Common.ToolboxC/Indictus.Common.ToolboxC/src/CollectionUtils.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Indictus.Common.ToolboxC
+{
+    class CollectionUtils
+    {
+    //    Public Shared Function TranslateDelimitedStringToCollection(ByVal sString As String, ByVal sDelimiter As String) As Collection
+
+    //    Dim coll As Collection = Nothing
+
+    //    Try
+    //        coll = New Collection
+    //        For Each s As String In Toolbox.StringUtils.Split(sString, sDelimiter)
+    //            coll.Add(s)
+    //        Next
+    //    Catch ex As Exception
+    //        Throw ex
+    //    End Try
+
+    //    Return coll
+
+    //End Function
+
+        public static ArrayList TranslateDelimitedStringToArrayList(string sString, string sDelimiter)
+        {            
+            ArrayList arr = new ArrayList();
+
+            try
+            {
+            }
+            catch (Exception ex) {
+                throw ex;
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ Common/Indictus.Common.ToolboxC/Indictus.Common.ToolboxC/src/StringUtils.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Indictus.Common.ToolboxC
+{
+    class StringUtils : BaseUtils
+    {
+            public String ConstructWhiteSpace(int iNrOfMoves) 
+            {
+                return "";
+            }                
+    }
+}
